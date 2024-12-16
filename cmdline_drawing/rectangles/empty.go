b407d483b5f5b19c53cd1d5dc94c1a050c360160
Mateo <mateobarac0712@gmx.de>
2024-12-16 21:31:48 +0100

    empty erledigt in schleifen (unlesbar)

--- cmdline_drawing/rectangles/empty.go
+++ cmdline_drawing/rectangles/empty.go
@@ -1,10 +1,27 @@
 package rectangles
 
+import "fmt"
+
 // Erwartet zwei Seitenlängen `height` und `width`.
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Der Rand des Rechtecks soll aus `#`-Zeichen bestehen, der Innenraum soll leer sein.
 func DrawEmptyRectangle(height, width int) {
-	// TODO
+	for i := 0; i < height; i++ {
+		if i == 0 || i == height-1 {
+			for i := 0; i < width; i++ {
+				fmt.Printf("#")
+			}
+		} else {
+			for i := 0; i < width; i++ {
+				if i == 0 || i == width-1 {
+					fmt.Printf("#")
+				} else {
+					fmt.Printf(" ")
+				}
+			}
+		}
+		fmt.Printf("\n")
+	}
 }
 
 // REMARKS
